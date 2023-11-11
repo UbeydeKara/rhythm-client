3f57ad60396a4611db82f884e3f1336e800cec1f
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-11-11 23:40:05 +0300

    theme customization

---
+++ src/theme/index.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import {useMemo} from 'react';
+// @mui
+import {CssBaseline} from '@mui/material';
+import {createTheme, ThemeProvider as MUIThemeProvider} from '@mui/material/styles';
+//
+import palette from './palette';
+import typography from './typography';
+import breakpoints from './breakpoints';
+import componentsOverride from './overrides';
+import shadows, {customShadows} from './shadows';
+
+// ----------------------------------------------------------------------
+
+ThemeProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default function ThemeProvider({children}) {
+    // TODO: theme will be editable later
+    const isLight = true;
+
+    const themeOptions = useMemo(() => ({
+        palette: isLight ? palette.light : palette.dark,
+        typography,
+        breakpoints,
+        shape: {borderRadius: 8},
+        shadows: isLight ? shadows.light : shadows.dark,
+        customShadows: isLight ? customShadows.light : customShadows.dark,
+    }), [isLight]);
+
+    const theme = createTheme(themeOptions);
+    theme.components = componentsOverride(theme);
+
+    return (<MUIThemeProvider theme={theme}>
+            <CssBaseline/>
+            {children}
+        </MUIThemeProvider>);
+}
