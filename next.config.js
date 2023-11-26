e3f475ffe748cde2fdc50fd0f3a1f111cb3e0ad7
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-11-27 00:17:55 +0300

    rewrite api url

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
             },
         ]
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://13.50.110.62:8080/api/v1/:path*'
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
